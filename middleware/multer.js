a00a1053e4e8246dcd7d8af70577fced101480f5
bhaskar <bhandari222ji@gmail.com>
2024-04-16 20:00:50 +0530

    multer updated

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,12 @@
 const multer  = require('multer');
 
-
 const storage = multer.diskStorage({
   
     destination: function (req, file, cb) {
       cb(null, './photos');
     },
     filename: function (req, file, cb) {
-      const name = Date.now()+'-'+file.originalname ;
-      cb(null, name );
+      cb(null, Date.now() + file.originalname);
     }
   })
   
